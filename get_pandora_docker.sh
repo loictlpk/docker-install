34a3c6977bacf03a29edd73c8e7da78d7819bc6f
loictlpk <88146484+loictlpk@users.noreply.github.com>
2024-12-06 12:12:53 +0100

    Update get_pandora_docker.sh

--- get_pandora_docker.sh
+++ get_pandora_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+git clone https://github.com/pandora-analysis/pandora
 docker pull ghcr.io/pandora-analysis/pandora:sha256-66fd9bd48682f59dd62530b8456759c474680f8221f5f492f71884263e58121a.sig
